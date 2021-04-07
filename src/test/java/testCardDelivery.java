e7f390c27dcdea6c9c4f117abcb9bd847064a727
anichkova <anichkova2016@yamdex.ru>
2021-04-07 22:48:15 +0300

    modif gradle.yml

--- src/test/java/testCardDelivery.java
+++ src/test/java/testCardDelivery.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -5,17 +6,18 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class testCardDelivery {
     LocalDate today = LocalDate.now();
     LocalDate newDate = today.plusDays(3);
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     String formatDateNew = newDate.format(formatter);
+    LocalDate NewWeek = today.plusDays(7);
+    String FormatWeek = NewWeek.format(formatter);
+    int dayPlusSeven = NewWeek.getDayOfMonth();
 
     @BeforeEach
     void setUp() {
@@ -26,6 +28,8 @@ public class testCardDelivery {
     @Test
     void shouldSubitRequest() {
         $("[data-test-id=city] input").setValue("Вологда");
+        $("[data-test-id=date] input").clear();
+        $("[data-test-id=date] input").setValue(formatter.format(newDate));
         $("[data-test-id=name] input").setValue("Сергей");
         $("[data-test-id=phone] input").setValue("+79212223344");
         $("[data-test-id=agreement]").click();
@@ -35,4 +39,5 @@ public class testCardDelivery {
         $("[data-test-id=notification] .notification__content").shouldHave(exactText("Встреча успешно" +
                 " забронирована на " + formatDateNew));
     }
+
 }
