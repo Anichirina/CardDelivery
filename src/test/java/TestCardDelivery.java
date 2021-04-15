518e41483af6f92e9f63a9aab0d0eac0417b085d
Ira Anichkova <anichkova2016@yandex.ru>
2021-04-15 17:14:25 +0300

    modified TestCardDelivery

--- src/test/java/TestCardDelivery.java
+++ src/test/java/TestCardDelivery.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -10,7 +11,7 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class testCardDelivery {
+public class TestCardDelivery {
     LocalDate today = LocalDate.now();
     LocalDate newDate = today.plusDays(3);
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
@@ -26,7 +27,7 @@ public class testCardDelivery {
     @Test
     void shouldSubitRequest() {
         $("[data-test-id=city] input").setValue("Вологда");
-        $("[data-test-id=date] input").clear();
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(formatter.format(newDate));
         $("[data-test-id=name] input").setValue("Сергей");
         $("[data-test-id=phone] input").setValue("+79212223344");
